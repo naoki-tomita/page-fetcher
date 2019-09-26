8a077f3823a1949311899d03ba158400f6ea5c8b
kojiro.ueda <mxcpy608@hotmail.com>
2019-09-26 13:57:42 +0900

    aaaa

--- handler.js
+++ handler.js
@@ -3,5 +3,5 @@ module.exports = async (event, response) => {
   const { url } = event.query;
   const fetchedResponse = await fetch(url);
   const body = await fetchedResponse.text();
-  response.status(200).headers({ "content-type": "text/plain" }).succeed(body);
+  response.status(200).headers({ "content-type": "text/plain", "Access-Control-Allow-Origin": "*" }).succeed(body);
 };
