e8a2c9f575d713062d21c849aae1efd2612703ef
kojiro.ueda <mxcpy608@hotmail.com>
2019-09-30 08:37:15 +0900

    Add type for handler.

---
+++ Handler.d.ts
@@ -0,0 +1,17 @@
+export interface FunctionEvent {
+  body: any;
+  headers: {
+    cookie: string;
+  },
+  method: "GET" | "POST" | "PUT" | "DELETE",
+  query: { [key: string]: string },
+  path: string
+}
+
+export interface FunctionContext {
+  value: number;
+  status(status: number): FunctionContext;
+  headers(headers: { [key: string]: string }): FunctionContext;
+  succeed(arg: any): void;
+  headerValues: { [key: string]: string };
+}
--- handler.js
+++ handler.js
@@ -1,7 +1,20 @@
-const fetch = require("node-fetch");
-module.exports = async (event, response) => {
+//@ts-check
+const nodeFetch = require("node-fetch");
+const fetch = nodeFetch.default;
+
+
+/**
+ * @param {import("./Handler").FunctionEvent} event
+ * @param {import("./Handler").FunctionContext} response
+ */
+module.exports = async function handle(event, response) {
   const { url } = event.query;
   const fetchedResponse = await fetch(url);
   const body = await fetchedResponse.text();
-  response.status(200).headers({ "content-type": "text/plain", "Access-Control-Allow-Origin": "*" }).succeed(body);
+  response.status(200)
+    .headers({
+      "content-type": "text/plain",
+      "Access-Control-Allow-Origin": "*"
+    })
+    .succeed(body);
 };
